0977b47ded0fa20cb42c7e08c7f8e10492db1b05
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2025-01-11 14:27:29 +0700

    fixed bug

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Spinner from "../components/spinner";
+import Spinner from "../components/Spinner";
 import CardRecipe from "../components/CardRecipe";
 
 export default function RecipeDetail() {
